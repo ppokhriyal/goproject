1b8432579f407c5349852e1be6d46a15033b2228
funix <funix@gmail.com>
2021-03-04 12:55:46 +0530

    Go Yaml

--- sample.go
+++ sample.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	vi := viper.New()
-	vi.SetConfigFile("sample.yml")
-	vi.ReadInConfig()
-	fmt.Println(vi.GetString("awsvpc.name"))
+type Engineer struct {
+	Id			string		`yaml:"id"`
+	Sector		int			`yaml:"sector"`
+	Tasks		[]string	`yaml:"tasks"`
+	DailyHours	[]int		`yaml:"dailyHours"`
+	Languages	map[string]float32	`yaml:"languages"`
+}
+
+func main(){
+	f := &Engineer{}
+	source,err := ioutil.ReadFile("sample.yaml")
+	if err != nil {
+		log.Println(err)
+	}
+	err = yaml.Unmarshal([]byte(source), &f)
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+	fmt.Println(f.DailyHours)
 }
