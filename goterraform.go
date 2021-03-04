c4a85a7986dfa7d840418db54e77cb0100df47b6
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2021-03-04 23:04:20 +0530

    Go Yaml

--- goterraform.go
+++ goterraform.go
@@ -22,16 +22,32 @@ func validate_region(regionselected string) bool {
 	}
 
 }
+//validate access key and secret key
+func validate_access_secret_key(accesskeys,secretkeys string) bool {
+	var status bool
+	status = false
+
+	if len(accesskeys) == 20 && len(secretkeys) == 40 {
+		status = true
+		return status
+	} else {
+		return status
+	}
+}
 //Structure of GoTerraform.Yaml
 type AWS struct{
 	ProjectName string	`yaml:"projectname"`
-	Region		string	`yaml:"region"`
+	AwsKey		map[string]string	`yaml:"awskey"`
+	BuildVpc	map[string]string	`yaml:"buildvpc"`
 }
 
 func main(){
 
 	//Vriables
 	var selected_region string
+	var selected_vpcname string
+	var selected_accesskey string
+	var selected_secretkey string
 
 	f := &AWS{}
 	source,err := ioutil.ReadFile("goterraform.yaml")
@@ -61,20 +77,39 @@ func main(){
 		fmt.Println("Creating Project : "+f.ProjectName)
 		os.MkdirAll(f.ProjectName,0755)
 	}
-	//check if region is pre-defined or it is requested for ask
-	if f.Region == "ask" {
-		fmt.Println("Region will be asked")
+	//build awskey/region
+	// 1. check for aws region 'ask'/valid_regionname
+	selected_region = f.AwsKey["region"]
+
+	if selected_region == "ask" {
+		fmt.Println("region need to asked")
 	} else {
-		selected_region = f.Region
+
 		validate_region_result := validate_region(selected_region)
-		if validate_region_result == false {
-			fmt.Println("\nError : Invalid region "+selected_region)
-			avregions := "Available Regions \n-----------------\n"+
-			"a. us-east-1\tb. us-east-2\n"+
-			"c. us-west-1\td. us-west-2"
-			fmt.Println(avregions)
-			
+		if validate_region_result == true {
+			fmt.Println("Selected Region  : "+selected_region)
+		} else {
+			fmt.Println("\nError: invalid US region "+selected_region)
+			valid_regions := "\nValid Regions in US\n-------------------\n"+
+			"a. us-east-1\t b. us-east-2\n"+
+			"c. us-west-1\t c. us-west-2"
+			fmt.Println(valid_regions)
+			os.Exit(1)
 		}
-		//fmt.Println("Selected Region  : "+selected_region)
 	}
+	// 2. check access/secret key
+	selected_accesskey = f.AwsKey["access_key"]
+	selected_secretkey = f.AwsKey["secret_key"]
+
+	selected_accessecretkey_result :=  validate_access_secret_key(selected_accesskey,selected_secretkey)
+	if selected_accessecretkey_result == true {
+		fmt.Println("Selected AWS Key : "+selected_accesskey)
+	} else {
+		fmt.Println("\nError: invalid Access/Secret key")
+		os.Exit(1)
+	}
+	//build vpc
+	selected_vpcname = f.BuildVpc["name"]
+	fmt.Println("VPC Name	 : "+selected_vpcname)
+
 }
