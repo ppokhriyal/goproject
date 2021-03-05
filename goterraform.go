91d981d38e67c0cf546d8cf8d3981f17ced84879
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2021-03-05 17:20:27 +0530

    Go Yaml

--- goterraform.go
+++ goterraform.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"gopkg.in/yaml.v2"
+	"strconv"
 )
 
 //validate selected region
@@ -41,10 +42,25 @@ func check_err(e error){
 	}
 }
 //Structure of GoTerraform.Yaml
-type AWS struct{
-	ProjectName string	`yaml:"projectname"`
-	AwsKey		map[string]string	`yaml:"awskey"`
-	BuildVpc	map[string]string	`yaml:"buildvpc"`
+type AWS struct {
+	Projectname string `yaml:"projectname"`
+	Awskey      struct {
+		Region    string `yaml:"region"`
+		AccessKey string `yaml:"access_key"`
+		SecretKey string `yaml:"secret_key"`
+	} `yaml:"awskey"`
+	Buildvpc struct {
+		Name               string `yaml:"name"`
+		CidrBlock          string `yaml:"cidr_block"`
+		EnableDNSSupport   string `yaml:"enable_dns_support"`
+		EnableDNSHostnames string `yaml:"enable_dns_hostnames"`
+	} `yaml:"buildvpc"`
+	Buildsubnet []struct {
+		Type             string `yaml:"type"`
+		Name             string `yaml:"name"`
+		CidrBlock        string `yaml:"cidr_block"`
+		AvailabilityZone string `yaml:"availability_zone"`
+	} `yaml:"buildsubnet"`
 }
 
 func main(){
@@ -71,32 +87,32 @@ func main(){
 	//check for project workspace exists or not
 	//if not then create new and if exists, remove
 	//and then create new.
-	_, err = os.Stat(f.ProjectName);
+	_, err = os.Stat(f.Projectname);
 
 	if err == nil {
 		//project is already available
 		//so remove it and create again
-		fmt.Println("Updating Project : "+f.ProjectName)
-		os.RemoveAll(f.ProjectName)
-		os.MkdirAll(f.ProjectName,0755)
+		fmt.Println("Updating Project : "+f.Projectname)
+		os.RemoveAll(f.Projectname)
+		os.MkdirAll(f.Projectname,0755)
 		//create project.tf and project_variable.tf file
 		pwd,_ := os.Getwd()
-		filemain_tf,err2 := os.Create(pwd+"/"+f.ProjectName+"/"+f.ProjectName+"_main.tf")
+		filemain_tf,err2 := os.Create(pwd+"/"+f.Projectname+"/"+f.Projectname+"_main.tf")
 		check_err(err2)
 		defer filemain_tf.Close()
 	} else {
 		//creating new project workspace
-		fmt.Println("Creating Project : "+f.ProjectName)
-		os.MkdirAll(f.ProjectName,0755)
+		fmt.Println("Creating Project : "+f.Projectname)
+		os.MkdirAll(f.Projectname,0755)
 		//create project.tf and project_variable.tf file
 		pwd,_ := os.Getwd()
-		filemain_tf,err2 := os.Create(pwd+"/"+f.ProjectName+"/"+f.ProjectName+"_main.tf")
+		filemain_tf,err2 := os.Create(pwd+"/"+f.Projectname+"/"+f.Projectname+"_main.tf")
 		check_err(err2)
 		defer filemain_tf.Close()
 	}
 	//build awskey/region
 	// 1. check for aws region 'ask'/valid_regionname
-	selected_region = f.AwsKey["region"]
+	selected_region = f.Awskey.Region
 
 	if selected_region == "ask" {
 		fmt.Println("region need to asked")
@@ -115,8 +131,8 @@ func main(){
 		}
 	}
 	// 2. check access/secret key
-	selected_accesskey = f.AwsKey["access_key"]
-	selected_secretkey = f.AwsKey["secret_key"]
+	selected_accesskey = f.Awskey.AccessKey
+	selected_secretkey = f.Awskey.SecretKey
 
 	selected_accessecretkey_result :=  validate_access_secret_key(selected_accesskey,selected_secretkey)
 	if selected_accessecretkey_result == true {
@@ -127,8 +143,9 @@ func main(){
 		" region = \""+selected_region+"\"\n"+
 		" access_key = \""+selected_accesskey+"\"\n"+
 		" secret_key = \""+selected_secretkey+"\"\n}\n"
+		
 		pwd,_ := os.Getwd()
-		fil,err := os.OpenFile(pwd+"/"+f.ProjectName+"/"+f.ProjectName+"_main.tf", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+		fil,err := os.OpenFile(pwd+"/"+f.Projectname+"/"+f.Projectname+"_main.tf", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
 		check_err(err)
 		if _, err := fil.Write([]byte(maintf)); err != nil {
 			log.Fatal(err)
@@ -142,7 +159,53 @@ func main(){
 		os.Exit(1)
 	}
 	//build vpc
-	selected_vpcname = f.BuildVpc["name"]
+	selected_vpcname = f.Buildvpc.Name
 	fmt.Println("VPC Name	 : "+selected_vpcname)
+	maintf := "#Create VPC\n"+
+	"resource \"aws_vpc\" \"custom_vpc\" {\n"+
+	 "cidr_block = \""+f.Buildvpc.CidrBlock+"\"\n"+
+	 "enable_dns_support = "+f.Buildvpc.EnableDNSSupport+"\n"+
+	 "enable_dns_hostnames = "+f.Buildvpc.EnableDNSHostnames+"\n"+
+	 "tags = {\n"+
+	  " Name = \""+f.Projectname+"_vpc\"\n}\n}\n"
 
-}
+	pwd,_ := os.Getwd()
+	fil,err := os.OpenFile(pwd+"/"+f.Projectname+"/"+f.Projectname+"_main.tf", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+	check_err(err)
+	if _, err := fil.Write([]byte(maintf)); err != nil {
+		log.Fatal(err)
+	}
+	if err := fil.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	//build subnet
+	buildsubnet_count := len(f.Buildsubnet)
+	count := 0
+	for count < buildsubnet_count {
+		
+		if f.Buildsubnet[count].Type == "public" {
+			maintf := "#Configure Public Subnet\n"+
+			"resource \"aws_subnet\" \"custom_publicsubnet_"+strconv.Itoa(count)+"\" {\n"+
+			" vpc_id = aws_vpc.custom_vpc.id\n"+
+			" cidr_block = \""+f.Buildsubnet[count].CidrBlock+"\"\n"+
+			" availability_zone = \""+f.Buildsubnet[count].AvailabilityZone+"\"\n"+
+			" tags = {\n \"Name\" = \""+f.Projectname+"_publicsubet_"+strconv.Itoa(count)+"\"\n}\n}\n"
+			
+			pwd,_ := os.Getwd()
+			fil,err := os.OpenFile(pwd+"/"+f.Projectname+"/"+f.Projectname+"_main.tf", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+			check_err(err)
+			if _, err := fil.Write([]byte(maintf)); err != nil {
+				log.Fatal(err)
+			}
+			if err := fil.Close(); err != nil {
+				log.Fatal(err)
+			}
+			
+		} else {
+			fmt.Println("Private it is")
+		}
+		count += 1
+	}
+	
+}	
